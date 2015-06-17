4d02d315639604a69b9b62314e4500fc121de3be
Timo Wischer <t.wischer@tu-bs.de>
2015-06-17 08:55:12 +0200

    Add copyright to files

--- Source/user/stdout.c
+++ Source/user/stdout.c
@@ -1,15 +1,19 @@
-//Stupid bit of code that does the bare minimum to make os_printf work.
-
 /*
- * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
- * Jeroen Domburg <jeroen@spritesmods.com> wrote this file. As long as you retain 
- * this notice you can do whatever you want with this stuff. If we meet some day, 
- * and you think this stuff is worth it, you can buy me a beer in return. 
- * ----------------------------------------------------------------------------
+ *   Copyright 2015, Timo Wischer <twischer@freenet.de>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
-
 #include "espmissingincludes.h"
 #include "osapi.h"
 #include "stdout.h"
